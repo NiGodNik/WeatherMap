5ec67e239e9a09c3b9a69f2d3088a07777d167e1
Nik <bogdanov.n.g@gmail.com>
2016-10-23 10:30:04 +0700

    New project

---
+++ app/src/main/java/com/nik/weathermap/WeatherMap.java
@@ -0,0 +1,13 @@
+package com.nik.weathermap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherMap extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_map);
+    }
+}
